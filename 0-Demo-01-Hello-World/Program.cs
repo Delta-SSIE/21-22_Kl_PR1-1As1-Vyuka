4c6ae6e08efd9ed593ac5325a2252a8a9e9a6689
Jan Koupil <jan.koupil@gmail.com>
2021-09-01 22:05:11 +0200

    Add project files.

---
+++ 0-Demo-01-Hello-World/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _0_Demo_01_Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
