92162f5db8e8d846ebd2e8b01a74bb79acd1f905
Jan Koupil <jan.koupil@gmail.com>
2021-09-17 11:18:48 +0200

    2. Lekce - C# a string

---
+++ 01-Uvod-01-Jak-se-jmenujes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01_Uvod_01_Jak_se_jmenujes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Tento řádek vypíše No nazdar
+            // Console.WriteLine("No nazdar");
+
+            /* Více-
+              řádkový
+              konimentář
+             */
+
+            //deklarace proměnné jmeno ("Bude se používat")
+            //string jmeno;
+
+            //inicializace proměnné jmeno ("Dám do ní poprvé hodnotu")
+            //jmeno = "Karel";
+
+
+            //deklarace a inicializace současně
+            //string jmeno = "Vít";
+
+            string jmeno;
+
+            Console.Write("Jak se jmenuješ: "); //vyzvu uživatele, aby zadal jméno
+            jmeno = Console.ReadLine(); //čeká na enter a vrátí do proměnné hodnotu z klávesnice
+
+            Console.WriteLine("Ať žije " + jmeno + "!");
+        }        
+    }
+}
