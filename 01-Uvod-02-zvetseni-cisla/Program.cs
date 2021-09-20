daa31ac146c24d49d8e55b3e044ba83453c8e58b
Jan Koupil <jan.koupil@gmail.com>
2021-09-20 14:41:45 +0200

    Lekce 20.9. - Datové typy a parsování

---
+++ 01-Uvod-02-zvetseni-cisla/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01_Uvod_02_zvetseni_cisla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // celé číslo = integer (anglicky)
+            //int cisloA = 15;
+            //Console.WriteLine(cisloA); //vypíše jako řetězec
+
+
+
+            // načti od uživatele číslo
+            Console.WriteLine("Zadej celé číslo:");
+            string nacteno = Console.ReadLine();
+
+            //převedu na celé číslo
+            //int cislo = int.Parse(nacteno);
+
+            //převedu na desetinné číslo
+            double desCislo = double.Parse(nacteno);
+
+            // zvětši ho o 1
+            //nacteno = nacteno + 1; //takhle nelze - spojí řetězce
+            //cislo = cislo + 1;
+            //cislo++;
+            desCislo = desCislo + 1;
+
+            // vypiš mu ho zpět
+
+            //Console.WriteLine("Po zvětšení vychází " + cislo);
+            Console.WriteLine("Po zvětšení vychází " + desCislo);
+
+            
+        }
+    }
+}
---
+++ 01-Uvod-04-Char-demo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01_Uvod_04_Char_demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 znak = character
+            char pismenko = ':';
+            char nacteno = Console.ReadKey().KeyChar;
+
+            Console.WriteLine();
+            Console.WriteLine("Stiskl jsi " + nacteno);
+
+            //Console.WriteLine("Stiskni libovolnou klávesu pro ukončení.");
+            //Console.ReadKey();
+
+        }
+    }
+}
