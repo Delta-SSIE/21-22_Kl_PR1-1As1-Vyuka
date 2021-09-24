fb92804d2be1c017ff5adccd711eff74c3626b9d
Jan Koupil <jan.koupil@gmail.com>
2021-09-24 11:10:22 +0200

    Lekce 24.9.

---
+++ 01-Uvod-04-Uprava/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_Uvod_04_Uprava
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Zadej délku strany čtverce: ");
+
+            string zadano = Console.ReadLine();
+            double strana = double.Parse(zadano);
+
+            double obsah = strana * strana;
+
+            //Console.WriteLine("Obsah čtverce o straně " + strana + " je " + obsah + ".");
+            Console.WriteLine($"Obsah čtverce o straně {strana} je {obsah}.");
+        }
+    }
+}
---
+++ 01-Uvod-05-Cisla-a-format/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01_Uvod_05_Cisla_a_format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pi = Math.PI;
+            Console.WriteLine($"Pí je {pi} .");
+
+            double zaokrouhleno = Math.Round(pi, 3); //zaokrouhli na tři místa
+            Console.WriteLine($"Zaokrouhleno je to {zaokrouhleno} .");
+
+            //formátovaný výpis
+            Console.WriteLine($"Lze zobrazit s formátováním jako {pi:0.00000}");
+        }
+    }
+}
