8f651be8338279240b2a6a83771fd6136e4a33b3
Jan Koupil <jan.koupil@gmail.com>
2021-10-04 12:39:31 +0200

    Lekce 4. 10. 2021

---
+++ 01-Uvod-08-reseni-testu/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01_Uvod_08_reseni_testu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Zadej první dopravní prostředek, kterým chceš cestovat:");
+            string dp1 = Console.ReadLine();
+
+            Console.WriteLine("Zadej druhý dopravní prostředek, kterým chceš cestovat:");
+            string dp2 = Console.ReadLine();
+
+            Console.WriteLine($"Napiš jak daleko chceš cestovat dopravním prostředkem {dp1}:");
+            string vstup1 = Console.ReadLine();
+            double vzdalenost1 = double.Parse(vstup1);
+
+            Console.WriteLine($"Napiš jak daleko chceš cestovat dopravním prostředkem {dp2}:");
+            string vstup2 = Console.ReadLine();
+            double vzdalenost2 = double.Parse(vstup2);
+
+            double celkem = vzdalenost1 + vzdalenost2;
+
+            Console.WriteLine($"Celková cesta dopravními prostředky {dp1} a {dp2} bude dlouhá {celkem} km.");
+        }
+    }
+}
---
+++ 02-Podm-02-Boolean/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _02_Podm_02_Boolean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool jeDrahe = true; //nový datový typ - jen true / false
+
+            if (jeDrahe)
+            {
+                Console.WriteLine("Tohle auto nechci!");
+            }
+            else
+            {
+                Console.WriteLine("je to popelnice, ale beru.");
+            }
+        }
+    }
+}
