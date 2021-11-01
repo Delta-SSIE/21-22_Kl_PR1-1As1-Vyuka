e8c70a9d6b387c969271961607135ce314d923b8
Jan Koupil <jan.koupil@gmail.com>
2021-11-01 21:37:14 +0100

    2021-11-01 - While

---
+++ 03-While-05-Kdy-ta-setka-padne/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace _03_While_05_Kdy_ta_setka_padne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generator RND
+            Random rnd = new Random();
+
+            int opakovani = 10000000;
+            int i = 0;
+
+            int max = 0;
+            int soucet = 0;
+
+            while (i < opakovani)
+            {
+
+                //udělám si počítadlo na hody
+                int hody = 0;
+
+                // proměnná, abych ji mohl použít v podmínce while
+                int cislo;
+
+                do
+                {
+                    //generuju náhodná čísla
+                    cislo = rnd.Next(1, 7);
+
+                    //zvýším počítadlo
+                    hody++;
+                }
+                while (cislo != 6);
+                //dokud to není 6
+
+                soucet += hody;
+
+                if (hody > max)
+                    max = hody;
+
+                //Console.WriteLine($"Potřeboval jsem {hody} pokusů.");
+
+                i++;
+            }
+
+            double prumer = (double)soucet / opakovani;
+
+            Console.WriteLine($"Průměrně čekám {prumer:0.00} krát, nejdéle {max} krát.");
+        }
+    }
+}
---
+++ 03-While-06-Soucet-cisel-v-intervalu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03_While_06_Soucet_cisel_v_intervalu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kde začít: ");
+            int dolniMez = int.Parse(Console.ReadLine());
+
+            Console.Write("Kde skončit: ");
+            int horniMez = int.Parse(Console.ReadLine());
+
+            int soucet = 0;
+            int pozice = dolniMez;
+
+            while (pozice <= horniMez)
+            {
+                soucet += pozice;
+                pozice++;
+            }
+
+            Console.WriteLine($"Součet všechn čísel od {dolniMez} do {horniMez} je {soucet}.");
+        }
+    }
+}
