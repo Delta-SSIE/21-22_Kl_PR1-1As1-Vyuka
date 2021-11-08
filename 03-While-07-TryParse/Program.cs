8814d2446da7ae841ee2dfd52fecf1802790144c
Jan Koupil <jan.koupil@delta-skola.cz>
2021-11-08 11:54:48 +0100

    2021-11-08 While, TryParse, Maximum

---
+++ 03-While-07-TryParse/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _03_While_07_TryParse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Napiš číslo: ");
+            //string vstup = Console.ReadLine();
+
+            //int cislo; //deklaruje proměnnou - při praví pro ni paměť
+            //if (int.TryParse(vstup, out cislo)) //zkusí parsovat, výsledek vloží do cislo
+            //    //vrátí true/false zda se povedlo
+            //{ 
+            //    Console.WriteLine($"Zapsal jsi {cislo}.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("To není číslo, troubo!");
+            //}
+
+
+            //int cislo; //deklaruje proměnnou - při praví pro ni paměť
+            //string vstup;
+
+            //do
+            //{
+            //    Console.WriteLine("Napiš číslo: ");
+            //    vstup = Console.ReadLine();
+            //}
+            //while (!int.TryParse(vstup, out cislo));
+
+            //Console.WriteLine($"Zapsal jsi {cislo}.");
+
+
+
+
+            int cislo; //deklaruje proměnnou - při praví pro ni paměť
+            bool success = true;
+
+            do
+            {
+                if (!success)
+                    Console.WriteLine("To není číslo!");
+
+                Console.WriteLine("Napiš číslo: ");
+
+                success = int.TryParse(Console.ReadLine(), out cislo);
+            }
+            while (!success);
+
+            Console.WriteLine($"Zapsal jsi {cislo}.");
+
+
+
+            //int cislo; //deklaruje proměnnou - při praví pro ni paměť
+            //Console.WriteLine("Napiš číslo: ");
+
+            //while ( !int.TryParse( Console.ReadLine(), out cislo) )
+            //{
+            //    Console.WriteLine("Řekl jsem číslo! Zadej číslo: ");                
+            //};
+
+            //Console.WriteLine($"Zapsal jsi {cislo}.");
+
+        }
+    }
+}
---
+++ 03-While-08-Minimum/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace _03_While_08_Minimum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // chceme najít minimum z 5 vložených čísel
+
+            //int pocet = 5; //počet průchodů
+            //int i = 0; //pokolikáté
+
+            //int maximum = int.MinValue;            
+
+            //while (i < pocet) 
+            //{
+            //    Console.Write($"Zadej {i}. číslo: ");
+
+            //    int cislo = int.Parse(Console.ReadLine());
+            //    if (cislo > maximum)
+            //        maximum = cislo;
+
+            //    i++;
+            //}
+
+            //Console.WriteLine($"Maximum bylo {maximum}.");
+
+
+            /* ukončení N */
+
+            //int i = 0; //pokolikáté
+            //int maximum = int.MinValue;
+
+            //string vstup = "";
+
+            //while (vstup.ToUpper() != "N")
+            //{
+            //    Console.Write($"Zadej {i}. číslo: ");
+
+            //    vstup = Console.ReadLine();
+
+            //    if (int.TryParse(vstup, out int cislo)) 
+            //    { 
+            //        if (cislo > maximum)
+            //            maximum = cislo;
+
+            //        i++;
+            //    }
+            //    else if (vstup.ToUpper() != "N")
+            //    {
+            //        Console.WriteLine("To není platný vstup");
+            //    }
+
+            //}
+
+            //Console.WriteLine($"Maximum bylo {maximum}.");
+
+
+            /* jiné ukončení */
+
+            int i = 0; //pokolikáté
+            int maximum = int.MinValue;
+
+            string vstup = "";
+
+            while (true)
+            {
+                Console.Write($"Zadej {i}. číslo: ");
+
+                vstup = Console.ReadLine(); //načtu
+
+                if (vstup.ToUpper() == "N") //ukončení při příkazu N
+                    break;
+
+                if (int.TryParse(vstup, out int cislo)) //zkusím převést
+                {
+                    if (cislo > maximum) //když lze převést, můžu aktualizovat maximum
+                        maximum = cislo;
+
+                    i++;
+                }
+                else
+                {
+                    Console.WriteLine("To není platný vstup"); //když nelze převést, varuju
+                }
+
+            }
+
+            Console.WriteLine($"Maximum bylo {maximum}.");
+
+        }
+
+    }
+}
