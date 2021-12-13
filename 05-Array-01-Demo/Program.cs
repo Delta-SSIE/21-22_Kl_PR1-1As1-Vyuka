5e376dfd29580810a9e2fb41e02f24af996bbd3c
Jan Koupil <jan.koupil@gmail.com>
2021-12-13 12:09:26 +0100

    2021-12-13 - Pole -úvod

---
+++ 05-Array-01-Demo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _05_Array_01_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string[] jmena = new string[5];
+
+            //jmena[0] = "Karel";
+            //jmena[1] = "Milan";
+            //jmena[2] = "Josef";
+            //jmena[3] = "Gertruda";
+            //jmena[4] = "Lída";
+
+            //for (int i = 0; i < jmena.Length; i++)
+            //{
+            //    Console.WriteLine("Na večeři přijde i " + jmena[i]);
+            //}
+
+            //Console.WriteLine(string.Join(", ", jmena));
+
+            int pocet = 5;
+
+            string[] jmena = new string[pocet];
+
+            for (int i = 0; i < jmena.Length; i++)
+            {
+                Console.Write("Zadej jméno: ");
+                jmena[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < jmena.Length; i++)
+            {
+                Console.WriteLine("Na večeři přijde i " + jmena[i]);
+            }
+
+
+        }
+    }
+}
