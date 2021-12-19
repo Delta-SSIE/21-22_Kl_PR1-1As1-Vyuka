e0a7bbad3727048fcbdb4d63ec02a7218a6e3013
Jan Koupil <jan.koupil@gmail.com>
2021-12-19 22:07:07 +0100

    Vánoční šifra

---
+++ 04-For-29-Demaskovani/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _04_For_29_Demaskovani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cestaVstup = @"vstup.txt";
+
+            if (!File.Exists(cestaVstup))
+            {
+                Console.WriteLine("File not found");
+                return;
+            }
+
+            string maskovano = File.ReadAllText(cestaVstup);
+
+            // sem prijde vas kod
+
+
+
+            
+        }
+    }
+}
