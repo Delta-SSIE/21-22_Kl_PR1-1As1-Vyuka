5f9ad4e364450a84a0a739d3c2d88f31c752da9b
Jan Koupil <jan.koupil@delta-skola.cz>
2022-01-10 11:26:57 +0100

    2022-01-10 - Lednová výzva

---
+++ 05-Array-Extra-Lednova-vyzva/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _05_Array_Extra_Lednova_vyzva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sifraVCelku = @"
+ABCDEFGIJKMOSTUVWXY
+AGIJKQSY
+ACDEGIKLNPQSUVWY
+ACDEGIJLOSUVWY
+ACDEGNOPQSUVWY
+AGILMNOSY
+ABCDEFGIKMOQSTUVWXY
+JMO
+ABEFGLNOQTVWXY
+ACDFHJKMPQTUVX
+ABCDGLMNOPRTV
+BCEFHKLNRTUW
+BCFGKMNPRSTWY
+BCENOPQRTVW
+ABDFGJKPRTU
+ABHIMOQRWX
+CEFGJLNOPQRSTUVWY
+IJMQUVX
+ABCDEFGLMNOQSUW
+AGILNQUY
+ACDEGIJMNQRSTUY
+ACDEGJNORSTUVXY
+ACDEGJQRSTUVWX
+AGIJKMOPSTVX
+ABCDEFGIJKLNOPRXY
+";
+            string[] sifraPole = new string[]
+            {
+                "ABCDEFGIJKMOSTUVWXY",
+                "AGIJKQSY",
+                "ACDEGIKLNPQSUVWY",
+                "ACDEGIJLOSUVWY",
+                "ACDEGNOPQSUVWY",
+                "AGILMNOSY",
+                "ABCDEFGIKMOQSTUVWXY",
+                "JMO",
+                "ABEFGLNOQTVWXY",
+                "ACDFHJKMPQTUVX",
+                "ABCDGLMNOPRTV",
+                "BCEFHKLNRTUW",
+                "BCFGKMNPRSTWY",
+                "BCENOPQRTVW",
+                "ABDFGJKPRTU",
+                "ABHIMOQRWX",
+                "CEFGJLNOPQRSTUVWY",
+                "IJMQUVX",
+                "ABCDEFGLMNOQSUW",
+                "AGILNQUY",
+                "ACDEGIJMNQRSTUY",
+                "ACDEGJNORSTUVXY",
+                "ACDEGJQRSTUVWX",
+                "AGIJKMOPSTVX",
+                "ABCDEFGIJKLNOPRXY",
+            };
+        }
+
+    }
+}
