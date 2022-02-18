2c8934ebddf656c57b3f9276c75fe82b5a716bb2
Jan Koupil <jan.koupil@gmail.com>
2022-02-18 12:38:32 +0100

    2022-02-18 Metody

---
+++ 06-Methods-03-Nacitani/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06_Methods_03_Nacitani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rok = NactiInt("Zadej rok narození:");
+            int mesic = NactiInt("Zadej rok narození:", "To se mi nelíbí");
+        }
+
+        static int NactiInt(string vyzva, string chyboveHlaseni = "To není celé číslo, zkus to znovu.")
+        {
+            Console.WriteLine(vyzva);
+
+            int cislo;
+            
+            while (!int.TryParse(Console.ReadLine(), out cislo))
+            {
+                Console.WriteLine(chyboveHlaseni);
+            }
+            return cislo;
+        }
+    }
+}
