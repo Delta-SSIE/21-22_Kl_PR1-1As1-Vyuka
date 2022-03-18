92ba4c060f82f2bfe03e30af1d4cf18224795cdc
Jan Koupil <jan.koupil@gmail.com>
2022-03-18 10:29:53 +0100

    Life - hotovo

--- 08-App-020-Life/Program.cs
+++ 08-App-020-Life/Program.cs
@@ -10,23 +10,70 @@ namespace _08_App_020_Life
             int vyska = 20;
             int pauza = 300;
 
-            bool[,] mapa = VytvorMapu(sirka, vyska);
+            //bool[,] mapa = VytvorMapu(sirka, vyska);            
+            bool[,] mapa = EditujMapu(sirka, vyska);
+            bool[,] staraMapa = new bool[vyska, sirka];
+            Console.Clear();
+            Console.CursorVisible = false;
 
             while (true)
             {
-                Vykresli(mapa);
+                Vykresli(staraMapa, mapa);
+                staraMapa = mapa;
                 mapa = PristiMapa(mapa);
                 //chvilicku pockej
                 System.Threading.Thread.Sleep(pauza); //kolik ms ma vlakno spat
             }
         }
-        static void Vykresli(bool[,] mapa)
+        static void VykresliEditor(bool[,] mapa, int poziceX, int poziceY)
         {
+            Console.Clear();
+            for (int y = 0; y < mapa.GetLength(0); y++)
+            {
+                for (int x = 0; x < mapa.GetLength(1); x++)
+                {
+                    if (x == poziceX && y == poziceY)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Black;
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                    }
 
+                    Console.Write(mapa[y,x] ? "#" : "-");
+
+                    if (x == poziceX && y == poziceY)
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Vykresli(bool[,] staraMapa, bool[,] novaMapa)
+        {
+            for (int y = 0; y < staraMapa.GetLength(0); y++)
+            {
+                for (int x = 0; x < staraMapa.GetLength(1); x++)
+                {
+                    if (novaMapa[y, x] != staraMapa[y, x])
+                    {
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(novaMapa[y, x] ? '#' : ' ');
+                    }
+                        
+                }                
+            }
         }
         static bool[,] VytvorMapu(int sirka, int vyska)
         {
+            bool[,] mapa = new bool[vyska, sirka];
 
+            mapa[2, 2] = true;
+            mapa[2, 3] = true;
+            mapa[2, 4] = true;
+
+            return mapa;
         }
         static bool BudeBunkaZiva(bool jeZiva, int pocetSousedu)
         {
@@ -94,6 +141,49 @@ namespace _08_App_020_Life
                 }
             }
             return pristi;
+        }
+
+        static bool[,] EditujMapu(int sirka, int vyska)
+        {
+            bool[,] mapa = new bool[vyska, sirka];
+
+            int poziceX = sirka / 2;
+            int poziceY = vyska / 2;
+
+            while (true)
+            {
+                VykresliEditor(mapa, poziceX, poziceY);
+                //čti vstup
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        poziceY--;
+                        if (poziceY < 0)
+                            poziceY = vyska - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        poziceY++;
+                        if (poziceY == vyska)
+                            poziceY = 0;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        poziceX--;
+                        if (poziceX < 0)
+                            poziceX = sirka - 1;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        poziceX++;
+                        if (poziceX == sirka)
+                            poziceX = 0;
+                        break;
+                    case ConsoleKey.Spacebar:
+                        mapa[poziceY, poziceX] = !mapa[poziceY, poziceX];
+                        break;
+                    case ConsoleKey.Enter:
+                        return mapa;
+                }                
+            }            
         }
     }
 }
