d5dc51a77ee5fccb094be6fe7ec0c1acb5f07e27
Jan Koupil <jan.koupil@gmail.com>
2022-03-21 08:37:44 +0100

    Enumerace

---
+++ 09-EnumException-020-Barva-karty/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09_EnumException_020_Barva_karty
+{
+    enum Barva { Srdce, Kary, Piky, Krize, Trefy = Krize };
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Barva moje = Barva.Krize;
+            Barva tvoje = Barva.Trefy;
+            if (moje == tvoje)
+                Console.WriteLine("Máme stejnou barvu");
+            else
+                Console.WriteLine("Každý máme jinou barvu");
+        }
+    }
+}
