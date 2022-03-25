fd4f0b6b38cc26ffbb5b06dc414e77f1b5febb79
Jan Koupil <jan.koupil@delta-skola.cz>
2022-03-25 10:31:08 +0100

    2022-03-25 OOP - uvod (kruznice)

---
+++ 10-OOP-010-Uvod-Kruznice-apod/Kruznice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10_OOP_010_Uvod_Kruznice_apod
+{
+    class Kruznice
+    {
+        public double Polomer;
+
+        public double Obvod()
+        {
+            return 2 * Math.PI * Polomer;
+        }
+    }
+}
---
+++ 10-OOP-010-Uvod-Kruznice-apod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10_OOP_010_Uvod_Kruznice_apod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kruznice k = new Kruznice();
+            k.Polomer = 1;
+            Console.WriteLine(k.Obvod());
+
+
+            Kruznice l = new Kruznice();
+            l.Polomer = 10;
+
+            Console.WriteLine(  l.Obvod() );
+
+            Kruznice m = new Kruznice();
+            //m = 10; tohle nepůjde
+
+            //Console.CursorVisible = true;
+
+        }
+    }
+}
